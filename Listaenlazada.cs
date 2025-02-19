38a6f3d2a91322a274941ebc0075bd202327dacf
Emloop12 <emloop.cl@gmail.com>
2025-02-18 21:53:59 -0600

    Update Listaenlazada.cs

--- Listaenlazada.cs
+++ Listaenlazada.cs
@@ -1,11 +1,11 @@
 using System;
 
-public class Nodo{
-    public int valor ;
+public class Nodo{ // creo la clase nodo
+    public int valor ;   // creo el valor y la referencia al siguiente nodo
     public Nodo siguiente;
 
     public Nodo (int valor){
-        this.valor = valor;
+        this.valor = valor;     // creo el constructor
         this.siguiente = null;
     }
 
@@ -13,45 +13,45 @@ public class Nodo{
 
 public class Listaenlazada{
 
-    Nodo principio, final;
+    Nodo principio, final; // creo el principio y el final y el largo de la lista
     public int largodelista;
 
-    public Listaenlazada(){
+    public Listaenlazada(){ // creo el constructor
         principio = null;
         final = null;
         largodelista = 0;
         
     }
 
-    public void AgregarAlprincipio(int valor){
+    public void AgregarAlprincipio(int valor){ //funcion para agregar al principio 
         Nodo nuevonodo = new Nodo(valor);
         if (principio == null)
-        {
-            principio = nuevonodo;  
+        {    
+            principio = nuevonodo;  // si principio es null el nuevo valor es principio y final
             final = nuevonodo;
-            largodelista++;
+            largodelista++; // el largo de la lista aumenta
         }
         else
         {
-            nuevonodo.siguiente = principio;
-            principio = nuevonodo;
+            nuevonodo.siguiente = principio; // el nuevo nodo apunta al principio
+            principio = nuevonodo; // y el principio ahora es igual al nuevo nodo
             largodelista++;
         }
         
     }
     
-    public void AgregarAlFinal(int valor)
+    public void AgregarAlFinal(int valor)  // agregar al final
     {
-        Nodo nuevonodo = new Nodo(valor);
+        Nodo nuevonodo = new Nodo(valor); 
         if (final == null)
         {
-            principio = nuevonodo;  
+            principio = nuevonodo;  // si esta vacia creo el nodo nuevo
             final = nuevonodo;
             largodelista++;
         }
         else
         {
-            final.siguiente = nuevonodo;  
+            final.siguiente = nuevonodo;   // inserto el nodo al final
             final = nuevonodo;  
             largodelista++; 
         }
@@ -60,51 +60,51 @@ public class Listaenlazada{
 
     public void AgregarNodoporindice(int valor, int posicion){
         Nodo nuevonodo = new Nodo(valor);
-        int contador = 2;
+        int contador = 2; // creo un contador
 
 
         if(posicion > largodelista + 1 || posicion <= 0){
             Console.WriteLine("Ingrese numeros validos, el tamaño de la lista es de" + largodelista);
-            return;
+            return;  // si el indice no es valido se manda un texto
         }
         
-        else if(posicion == 1){
+        else if(posicion == 1){ // si posicion es 1 se inserta al inicio 
             if (principio == null)
         {
             principio = nuevonodo;  
-            final = nuevonodo;
+            final = nuevonodo;  // si esta vacio se crea el nodo
             largodelista++;
         }
         else
         {
-            nuevonodo.siguiente = principio;
+            nuevonodo.siguiente = principio; // si no esta vacio se inserta al inicio
             principio = nuevonodo;
             largodelista++;
         }
             return;
 
         }
-        else if (posicion > 1 && posicion <= largodelista + 1){
+        else if (posicion > 1 && posicion <= largodelista + 1){ // insertar enntre el principio y el final
             Nodo nodoactual = principio.siguiente;
-            Nodo nodoanterior = principio;
+            Nodo nodoanterior = principio;  // creo nodo anterior y nodo actual
         
-            while(nodoactual != null && posicion > 1){
+            while(nodoactual != null && posicion > 1){ // si posicion es valido y no he recorrido todo el nodo
                 
-                if(contador == posicion){
-                    nodoanterior.siguiente = nuevonodo;
+                if(contador == posicion){ // si el contador que tengo es igual a la posicion
+                    nodoanterior.siguiente = nuevonodo;  // se inserta el nodo entre anterior y actual
                     nuevonodo.siguiente = nodoactual;
                     largodelista++;
                     return;
                 }
                 nodoactual = nodoactual.siguiente;
-                nodoanterior = nodoanterior.siguiente;
+                nodoanterior = nodoanterior.siguiente; // se pasa al siguiente nodo anterior y al siguiente actual
                 contador++;
             }
 
-            if(posicion == largodelista + 1){
+            if(posicion == largodelista + 1){ // si quiero insertar al final
                
             final.siguiente = nuevonodo;  
-            final = nuevonodo;  
+            final = nuevonodo;   // inserto el nodo al final
             largodelista++;
             }
         }
@@ -115,12 +115,12 @@ public class Listaenlazada{
 
     public void EliminarPorIndice(int posicion)
     {
-        if (principio == null)
+        if (principio == null) // si no hay nodos se manda un texto
         {
             Console.WriteLine("La lista está vacía.");
             return;
         }
-        if (posicion == 1)
+        if (posicion == 1)  //si la posicion es 1 se elimina
         {
 
             principio = principio.siguiente;
@@ -128,14 +128,14 @@ public class Listaenlazada{
             return;
         }
         
-        Nodo actual = principio;
+        Nodo actual = principio; // se crean 2 nodos uno actual y uno anterior
         Nodo anterior = null;
         int contador = 1;
         
-        while (actual != null && contador < posicion)
+        while (actual != null && contador < posicion) // recorro toda la lista hata la posicion deseada
         {
-            anterior = actual;
-            actual = actual.siguiente;
+            anterior = actual; 
+            actual = actual.siguiente;  
             contador++;
         }
         
@@ -145,11 +145,11 @@ public class Listaenlazada{
             return;
         }
         
-        anterior.siguiente = actual.siguiente;
+        anterior.siguiente = actual.siguiente; // borro el actual
         largodelista--;
         
-        if (actual == final)
-        {
+        if (actual == final) // si actual era el ultimo final ahora es igual al anterior
+        { 
             final = anterior;
         }
     }
@@ -159,14 +159,14 @@ public class Listaenlazada{
 
     public void Imprimir()
     {
-        if (principio == null)
+        if (principio == null) // si esta vacia mando un mensaje
         {
             Console.WriteLine("La lista está vacía.");
             return;
         }
 
         Nodo nodoActual = principio;
-        while (nodoActual != null)
+        while (nodoActual != null)  // recorro toda la lista hasta imprimir
         {
             Console.Write(nodoActual.valor + " ");
             nodoActual = nodoActual.siguiente;
@@ -188,30 +188,30 @@ class Program
         int numero = 0;
         int valor = 0;
         int posicion = 0;
-       Listaenlazada Listaenlazada = new Listaenlazada(); 
+       Listaenlazada Listaenlazada = new Listaenlazada();  // inicializo la lista
 
 
         do{
     Console.WriteLine("1. añadir al inicio de la lista ");
-    Console.WriteLine("2. añadir al final de la lista"); 
+    Console.WriteLine("2. añadir al final de la lista");  // creo el menu
     Console.WriteLine("3. añadir por posicion");
     Console.WriteLine("4. eliminar por posicion");
     Console.WriteLine("5. eliminar inicio");
     Console.WriteLine("6. eliminar final");
     Console.WriteLine("7. imprimir");
-    numero = int.Parse(Console.ReadLine()); 
+    numero = int.Parse(Console.ReadLine()); // pido un numero
     if(numero == 1){
-        Console.WriteLine("ingresa valor: ");
+        Console.WriteLine("ingresa valor: "); // veo que numero concuerda con el menu
     valor = int.Parse(Console.ReadLine()); 
         Listaenlazada.AgregarAlprincipio(valor);
     }
     else if(numero == 2){
 
         Console.WriteLine("ingresa valor: ");
-        valor = int.Parse(Console.ReadLine()); 
+        valor = int.Parse(Console.ReadLine());  
         Listaenlazada.AgregarAlFinal(valor);
     }
-     else if(numero == 3){
+     else if(numero == 3){                          // lamo las funciones
         Console.WriteLine("ingresa valor: ");
         valor = int.Parse(Console.ReadLine()); 
          Console.WriteLine("ingresa posicion: ");
@@ -236,7 +236,7 @@ class Program
         Listaenlazada.Imprimir();
     }
 
-    Console.WriteLine("quieres seguir con el programa?s/n"); 
+    Console.WriteLine("quieres seguir con el programa?s/n");  // verifico si quiero seguir con el programa
     sn = Console.ReadLine();
     if(sn != "n"){
         estado = true;
@@ -245,7 +245,7 @@ class Program
         estado = false;
     }
     }
-    while(estado);
+    while(estado); // uso un do-while para el menu
 
     }
 
